eaf7a0ee3caca9d3d5356529cae2272b2f2a2512
Abdul Matin <matinict@gmail.com>
2018-03-29 12:49:58 +0600

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2018 matinict@gmail.com. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'matinict_ku_IQ',
+    __DIR__
+);
